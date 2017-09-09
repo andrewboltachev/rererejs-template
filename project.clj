61a62635e4258953cc4ba5468bfc385949f89c87
Andrei Boltachev <andrewboltachev@gmail.com>
2017-09-09 19:36:47 +0300

    name change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rererejs/lein-template "0.1.0-SNAPSHOT"
+(defproject ru.andrewboltachev.rererejs/lein-template "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
